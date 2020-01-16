7d20fb0e92f7e35ed8b7f582118efbbaef96255d
Danielle Manguele Kathleen <ue2189fu@e2381-as1664963.century.edu>
2020-01-16 11:23:59 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-for(var i= 0; i<100;i++){
+
  var request= new XMLHttpRequest();
- request.open( 'GET','data.txt',false);
- request.send();
- console.log(request);
- // document.getElementById('demo').innerHTML=request.responseText;
-   if(request.status===200 && request.readyState===4){
-    console.log(request)
-    document.writeln(request.responseText);
-   }
-
-
-}
+ request.open( 'GET','data.txt');
+ 
+ request.onreadystatechange=function(){
+   // document.getElementById('demo').innerHTML=request.responseText;
+    if(request.status===200 && request.readyState===4){
+     console.log(request)
+     document.writeln(request.responseText);
+    }
+  }
+  request.send();
