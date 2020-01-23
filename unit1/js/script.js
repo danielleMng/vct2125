2fe3260903ab426cc3ace9bdf1cd3addabf25425
danielle Manguele <danielemanguele@gmail.com>
2020-01-23 13:20:39 -0600

    script content deleted

--- unit1/js/script.js
+++ unit1/js/script.js
@@ -1,13 +1,3 @@
 
- var request= new XMLHttpRequest();
- request.open( 'GET','data.txt',false);
- request.send();
- console.log(request);
- // document.getElementById('demo').innerHTML=request.responseText;
-   if((request.status===200) && (request.readyState===4)){
-    console.log(request)
-    document.writeln(request.responseText);
-   }
-
 
 
