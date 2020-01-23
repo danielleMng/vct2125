ad60a7b1b62035ab7a29ce66ef06e3e4351c1721
danielle Manguele <danielemanguele@gmail.com>
2020-01-23 14:11:32 -0600

    changes added

--- unit1/js/script.js
+++ unit1/js/script.js
@@ -9,8 +9,10 @@ request.onreadystatechange=function(){
 
     if((request.readyState===4) && (request.status===200)){
      
-     var modify=document.getElementsByTagName('ul')[1].getElementsByTagName('li');
-     modify[2].innerHTML=request.responseText;
+     var modify=document.getElementsByTagName('li');
+     for(var i=0; i<modify.length;i++){
+       modify[i].innerHTML= request.responseText;
+     }
     }
 }
 request.send();
