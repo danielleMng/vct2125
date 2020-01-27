2ec52e17078b77a13e08a1e41cddc1537945933d
danielle Manguele <danielemanguele@gmail.com>
2020-01-27 14:26:00 -0600

    adding styles to my css

--- unit1/js/scriptsearch.js
+++ unit1/js/scriptsearch.js
@@ -1,13 +1,18 @@
-$.getJSON('data.json', function(data){
-    var output='<ul class="searchresults">';
-    $.each(data,function(key,val){
-        output+='<li>';
-        output+='<h2>'+ val.name+'</h2>';
-        output+='<img src="images/'+val.name+'.jpg" alt=/>';
-        output+='<h2>'+ val.year+'</h2>';
-        output+='</li>';
-
+$('#search').keyup(function(){
+    var searchField=$('#search').val();
+    var myExp= new RegExp(searchField,"i");
+    $.getJSON('data.json', function(data){
+        var output='<ul class="searchresults">';
+        $.each(data,function(key,val){
+            if(val.name.search(myExp)!=-1){
+                output+='<li>';
+                output+='<h2>'+ val.name+'</h2>';
+                output+='<img src="images/'+val.name+'.jpg" alt=/>';
+                output+='<h2>'+ val.year+'</h2>';
+                output+='</li>';
+            }
+        });
+        output+='</ul>';
+        $("#update").html(output);
     });
-    output+='</ul>';
-    $("#update").html(output);
 });
