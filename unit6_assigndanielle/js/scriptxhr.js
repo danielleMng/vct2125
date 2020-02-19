dc1f066375bfc3895a416a6e1f450d080bc73119
Danielle Manguele Kathleen <ue2189fu@e2381-as1664963.century.edu>
2020-02-19 10:36:33 -0600

    unit 6 assignment stated and files created

---
+++ unit6_assigndanielle/js/scriptxhr.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+(function(){
+    var url = "https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=API_KEY";
+    var apiKey="b9169be6ca824b9781734c8c52a01ead";
+    var httpRequest;
+
+    function makeRequest(){
+    
+    }
+})();
